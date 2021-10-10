8faab73114e0630a01c3451647f1b289e32d8aeb
EthanColin <72448951+EthanColin@users.noreply.github.com>
2021-10-11 02:27:04 +0800

    Update process.php

--- Project/process.php
+++ Project/process.php
@@ -1,3 +1,7 @@
 <?php
-    echo "Hello World!";
+    if(!empty($_POST['sid']) && !empty($_POST['cat']) && !empty($_POST['quantity']) && !empty($_POST['total']) && !empty($_POST['date'])){
+        echo "<p>All fields are filled</p>";
+    }else{
+        echo "<p>Please fill in <b>ALL</b> the details</p>";
+    }
 ?>
