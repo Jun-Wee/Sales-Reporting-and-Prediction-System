be2ca754fabdc52f873f5eb8147960863f5e5751
EthanColin <ethan_colinlaw@hotmail.com>
2021-10-11 14:38:16 +0800

    Update process.php

--- Project/process.php
+++ Project/process.php
@@ -2,6 +2,6 @@
     if(!empty($_POST['sid']) && !empty($_POST['cat']) && !empty($_POST['quantity']) && !empty($_POST['total']) && !empty($_POST['date'])){
         echo "<p>All fields are filled</p>";
     }else{
-        echo "<p>Please fill in <b>ALL</b> the details</p>";
+        echo "<p>Please fill in <b>ALL</b> the details. Thank you.</p>";
     }
 ?>
