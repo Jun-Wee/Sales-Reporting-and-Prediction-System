7e077220482bf487c270cca4d7b293ebe91790ff
EthanColin <ethan_colinlaw@hotmail.com>
2021-10-12 22:58:39 +0800

    Fix Indentation
    Edit the values for options for processing purposes.

--- Project/edit.php
+++ Project/edit.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,73 +21,91 @@ function myFunction() {
 
 <body>
 <section>
-<div>
-<h2>Sales table</h2>
-<table id="edit_table">
-<tr>
-  <th>No</th>
-  <th>Category</th>
-  <th>Product</th>
-	<th>Price</th>
-	<th>Date</th>
-</tr>
-<tr>
-  <td>1</td>
-  <td>Device</td>
-  <td>Mouse</td>
-	<td>55.99</td>
-	<td>22/9/2021</td>
-	<td><button onclick="myFunction()">Edit</button><button onclick="myFunction()">Delete</button></td>
-</tr>
-</table>
-</div>
+  <div>
+    <h2>Sales table</h2>
+    <table id="edit_table">
+      <tr>
+        <th>No</th>
+        <th>Category</th>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Date</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Device</td>
+        <td>Mouse</td>
+        <td>55.99</td>
+        <td>22/9/2021</td>
+        <td><button onclick="myFunction()">Edit</button><button onclick="myFunction()">Delete</button></td>
+      </tr>
+    </table>
+  </div>
 </section>
 
-<aside>
-<div>
-<h2>Add sales</h2>
-<form action = "">
-<fieldset>
-<legend>Add sales</legend>
-<label for="id">Sales Id: </label>
-<input type="text" id="sid" name="sid" value="e.g. 1234"><br>
-<label for="cat">Category: </label>
-<select id="cat" name="cat">
-  <optgroup label="Health">
-    <option value="vitamins">Vitamins</option>
-    <option value="firstaid">First Aid</option>
-    <option value="facemask">Face Masks & Gloves</option>
-    <option value="eye&earcare">Eye & Ear Care</option>
-  <optgroup label="Personal Care">
-    <option value="handwash">Hand Wash & Sanitizer</option>
-    <option value="bathcare">Bath Care</option>
-    <option value="femininecare">Feminine Care</option>
-    <option value="oralcare">Oral Care</option>
-  <optgroup label="Cosmetics">
-    <option value="cotton">Cotton</option>
-    <option value="fragance">Fragance</option>
-    <option value="face">Face</option>
-    <option value="lips">Lips</option>
-  <optgroup label="Baby Care">
-    <option value="babydaipers">Baby Daipers</option>
-    <option value="babyfood">Baby Food</option>
-    <option value="babywipes">Baby Wipes</option>
-</select><br>
+  <aside>
+    <div>
+      <h2>Add sales</h2>
+      <form action = "">
+        <fieldset>
+          <legend>Add sales</legend>
 
-  <label for="quantity">Quantity: </label> 
-  <input type="text" id="quantity" name="quantity" value="e.g. 99"><br>
-  <label for="total">Total: </label> 
-  <input type="text" id="total" name="total" value="e.g. 88.88"><br>
-  <label for="date">Date: </label> 
-  <input type="date" id="date" name="date" value="<?php echo date("Y-m-d")?>"><br>
-  <?php 
-  // echo date("Y-m-d")
-  ?>
-  <input type="button" onclick="myFunction()" value="Add">
-</fieldset>
-</form>
-</div>
-</aside>
+          <div>
+            <label for="id">Sales Id: </label>
+            <input type="text" id="sid" name="sid" value="e.g. 1234">
+          </div>
+
+          <div>
+            <label for="cat">Product: </label>
+            <select id="cat" name="cat">
+              <optgroup label="Health">
+                <option value="Health,vitamins">Vitamins</option>
+                <option value="Health,firstaid">First Aid</option>
+                <option value="Health,facemask">Face Masks & Gloves</option>
+                <option value="Health,eye&earcare">Eye & Ear Care</option>
+              <optgroup label="Personal Care">
+                <option value="Personal Care,handwash">Hand Wash & Sanitizer</option>
+                <option value="Personal Care,bathcare">Bath Care</option>
+                <option value="Personal Care,femininecare">Feminine Care</option>
+                <option value="Personal Care,oralcare">Oral Care</option>
+              <optgroup label="Cosmetics">
+                <option value="Cosmetics,cotton">Cotton</option>
+                <option value="Cosmetics,fragance">Fragance</option>
+                <option value="Cosmetics,face">Face</option>
+                <option value="Cosmetics,lips">Lips</option>
+              <optgroup label="Baby Care">
+                <option value="Baby Care,babydaipers">Baby Daipers</option>
+                <option value="Baby Care,babyfood">Baby Food</option>
+                <option value="Baby Care,babywipes">Baby Wipes</option>
+            </select>
+          </div>
+
+          <div>
+            <label for="quantity">Quantity: </label> 
+            <input type="text" id="quantity" name="quantity" value="e.g. 99">
+          </div>
+
+          <div>
+            <label for="total">Total: </label> 
+            <input type="text" id="total" name="total" value="e.g. 88.88">
+          </div>
+
+          <div>
+            <label for="date">Date: </label> 
+            <input type="date" name="date" id="date" value="<?php echo date('Y-m-d');?>">
+            <?php 
+              // echo date("d-M-Y")
+            ?>
+          </div>
+
+          <div>
+            <input type="button" onclick="myFunction()" value="Add">
+          </div>
+
+        </fieldset>
+      </form>
+    </div>
+  </aside>
 </body>
 </html>
 
