e1cff1bc26b6aa4c71810bf85e73b3a59cc56d2c
“seejc” <58907677+seejc@users.noreply.github.com>
2021-10-13 22:06:18 +0800

    added login page and login_process

--- Project/include/header.php
+++ Project/include/header.php
@@ -1,12 +1,21 @@
-<?php echo'
-<header>
-<div class="navigationbar">
-<nav>
-	<a href="#manage">Sales</a>
-	<a href="#dashboard">Dashboard</a>
-	<a href="#analysis">Inventory</a>
-	<a href="#analysis">Analysis</a>
-</nav>
-</div>
-</header>
-'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title> PHP </title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="../style/style.css" type="text/css" />
+  <meta name="author" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+	<header>
+		<div class="navigationbar">
+			<nav>
+				<a href="#manage">Sales</a>
+				<a href="#dashboard">Dashboard</a>
+				<a href="#analysis">Inventory</a>
+				<a href="#analysis">Analysis</a>
+			</nav>
+		</div>
+	</header>
