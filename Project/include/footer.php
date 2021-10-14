22a310abb0e937f95641f1086ef12dc16658f3f4
Jun-Wee <tjunwee766@gmail.com>
2021-10-14 14:48:15 +0800

    Modify footer.php and style.css

--- Project/include/footer.php
+++ Project/include/footer.php
@@ -1,8 +1,14 @@
+<html>
 <link rel="stylesheet" href="../style/style.css" type="text/css" />
-
-
-<footer>
-    <div class="footer-bottom">
-        <p> &copy; People Health Pharmacy (PHP) Inc. | Designed by SWE20001 Lab 1 - Group 3|<a href="FAQ"> FAQ</a> </p>
+<footer>    
+<div class="footer-bottom">
+        <div class="footer-choose"> 
+            <h4>User Manual</h4>
+            <h4>FAQ</h4> 
+            <h4>Online Shop</f4>
+        </div>
+        <p> &copy; People Health Pharmacy (PHP) Inc. | Designed by SWE20001 Lab 1 - Group 3</p>
     </div>
+    
 </footer>
+</html>
