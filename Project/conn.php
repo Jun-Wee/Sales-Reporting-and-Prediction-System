a9ba5962a871c5976f0e620d12dcf5d87788b4fa
EthanColin <ethan_colinlaw@hotmail.com>
2021-10-16 04:25:20 +0800

    Update conn.php

--- Project/conn.php
+++ Project/conn.php
@@ -5,10 +5,15 @@ $password = "";
 $database = "phpsreps";
 
 // Create connection
+$conn = new mysqli($servername, $username, $password);
+
+mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS $database");
+
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
