331041139c3e4215f1505f4ee9ab267480ea7ba0
EthanColin <ethan_colinlaw@hotmail.com>
2021-10-16 04:44:47 +0800

    Update salesmodule.php

--- Project/salesmodule.php
+++ Project/salesmodule.php
@@ -3,7 +3,8 @@
   if(!$_SESSION["login_status"] == true){
     header("Location:login_page.php");
   }
-  ?>
+  include "conn.php";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,101 +13,109 @@
 <meta name="keywords" content="HTML5, tags"/>
 <title>Manage module</title>
 <link href="style/style.css" rel="stylesheet" type="text/css" />
-<?php include("include/header.php");?>
 </head>
 
-<script>
-function myFunction() {
-  var table = document.getElementById("edit_table");
-  var row = table.insertRow(2);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
-}
-</script>
-
+<?php include("include/header.php");?>
 <body>
-<section>
-  <div>
-    <h2>Sales table</h2>
-    <table id="edit_table">
-      <tr>
-        <th>No</th>
-        <th>Category</th>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Date</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>Device</td>
-        <td>Mouse</td>
-        <td>55.99</td>
-        <td>22/9/2021</td>
-        <td><button onclick="myFunction()">Edit</button><button onclick="myFunction()">Delete</button></td>
-      </tr>
-    </table>
-  </div>
-</section>
+  <section>
+    <div>
+      <h2>Sales table</h2>
+      <table style="border: 2;">
+        <tr>
+          <td>Sales ID</td>
+          <td>Category</td>
+          <td>Product</td>
+          <td>Total</td>
+          <td>Date</td>
+        </tr>
+        <?php
+
+        $records = mysqli_query($conn, "SELECT sales.sales_id, category.category_name, product.product_name, product.price, sales.date, sales_list.quantity * product.price AS total
+        FROM (((sales_list
+        INNER JOIN sales ON sales_list.sales_id=sales.sales_id)
+        INNER JOIN product ON sales_list.product_id=product.product_id)
+        INNER JOIN category on product.category_id=category.category_id) ORDER BY sales_id;"); // fetch data from database
+          
+        if($records){
+          while($data = mysqli_fetch_array($records))
+          {
+        ?>
+        <tr>
+          <td><?php echo $data['sales_id']; ?></td>
+          <td><?php echo $data['category_name']; ?></td>
+          <td><?php echo $data['product_name']; ?></td>    
+          <td><?php echo $data['total']; ?></td>    
+          <td><?php echo $data['date']; ?></td>    
+          <td><a href="editsales.php?id=<?php echo $data['sales_id']; ?>" name="edit">Edit</a></td>
+          <td><a href="deletesales.php?id=<?php echo $data['sales_id']; ?>" name="delete">Delete</a></td>
+        </tr>	
+        <?php
+          }
+        }
+        ?>
+      </table>
+    </div>
+  </section>
 
   <aside>
     <div>
       <h2>Add sales</h2>
-      <form action = "">
+      <form action = "addsales.php" method="POST">
         <fieldset>
           <legend>Add sales</legend>
 
           <div>
             <label for="id">Sales Id: </label>
-            <input type="text" id="sid" name="sid" value="e.g. 1234">
+            <input type="text" id="sid" name="sid" value="AUTO"> 
           </div>
 
           <div>
             <label for="cat">Product: </label>
             <select id="cat" name="cat">
+              <option value="" disabled selected>Select Product</option>
               <optgroup label="Health">
                 <option value="1,1">Vitamins</option>
                 <option value="1,2">First Aid</option>
                 <option value="1,3">Face Masks & Gloves</option>
                 <option value="1,4">Eye & Ear Care</option>
               <optgroup label="Personal Care">
-                <option value="2,1">Hand Wash & Sanitizer</option>
-                <option value="2,2">Bath Care</option>
-                <option value="2,3">Feminine Care</option>
-                <option value="2,4">Oral Care</option>
+                <option value="2,5">Hand Wash & Sanitizer</option>
+                <option value="2,6">Bath Care</option>
+                <option value="2,7">Feminine Care</option>
+                <option value="2,8">Oral Care</option>
               <optgroup label="Cosmetics">
-                <option value="3,1">Cotton</option>
-                <option value="3,2">Fragance</option>
-                <option value="3,3">Face</option>
-                <option value="3,4">Lips</option>
+                <option value="3,9">Cotton</option>
+                <option value="3,10">Fragance</option>
+                <option value="3,11">Face</option>
+                <option value="3,12">Lips</option>
               <optgroup label="Baby Care">
-                <option value="4,1">Baby Daipers</option>
-                <option value="4,2">Baby Food</option>
-                <option value="4,3">Baby Wipes</option>
+                <option value="4,13">Baby Daipers</option>
+                <option value="4,14">Baby Food</option>
+                <option value="4,15">Baby Wipes</option>
             </select>
           </div>
 
           <div>
             <label for="quantity">Quantity: </label> 
-            <input type="text" id="quantity" name="quantity" value="e.g. 99">
+            <input type="number" id="quantity" name="quantity" value="1">
           </div>
 
           <div>
             <label for="total">Total: </label> 
-            <input type="text" id="total" name="total" value="e.g. 88.88">
+            <input type="number" id="total" name="total" value="1">
           </div>
 
           <div>
             <label for="date">Date: </label> 
-            <input type="date" name="date" id="date" value="<?php echo date('Y-m-d');?>">
+            <input type="date" name="date" id="date" value="<?php date_default_timezone_set('Asia/Kuching'); echo date('Y-m-d');?>">
             <?php 
               // echo date("d-M-Y")
             ?>
           </div>
 
           <div>
-            <input type="button" onclick="myFunction()" value="Add">
+            <!-- <input type="button" onclick="myFunction()" value="Add"> -->
+            <input type="submit" value="Submit">
           </div>
 
         </fieldset>
