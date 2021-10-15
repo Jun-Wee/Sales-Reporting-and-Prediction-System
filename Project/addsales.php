21a5bc0a36c012344ac9acfa63e80f226f89bd53
EthanColin <ethan_colinlaw@hotmail.com>
2021-10-16 04:41:21 +0800

    Update addsales.php

--- Project/addsales.php
+++ Project/addsales.php
@@ -4,19 +4,12 @@
     if(!empty($_POST['sid']) && !empty($_POST['cat']) && !empty($_POST['quantity']) && !empty($_POST['total']) && !empty($_POST['date'])){
         $sid = $_POST['sid'];
         $product = $_POST['cat'];
-        $qty = $_POST['quantity'];
-        $total = $_POST['total'];
-        $date = $_POST['date'];
-
         $product_data = explode(",", $product);
         $category_id = $product_data[0];
         $product_id = $product_data[1];
-
-        echo "Sales ID: $sid <br>";
-        echo "Product Id: $product_id <br>";
-        echo "Category ID: $category_id <br>";
-        echo "Total: $total <br>";
-        echo "Date: $date <br>";
+        $qty = $_POST['quantity'];
+        $total = $_POST['total'];
+        $date = $_POST['date'];
 
         $sql = "INSERT INTO `sales` (`date`) VALUES ('$date');";
         $last_id = $conn->insert_id; 
@@ -28,6 +21,10 @@
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }else{
-        echo "<p>Please fill in <b>ALL</b> the details. Thank you.</p>";
+        if(empty($_POST['cat'])){
+            echo "<p>Please select a product to add. Thank you.</p>";
+        }else{
+            echo "<p>Please fill in <b>ALL</b> the details. Thank you.</p>";
+        }
     }
 ?>
