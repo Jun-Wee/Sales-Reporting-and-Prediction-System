54b8676ed01e356652de76dab68852aec8b34796
jentz2909 <cjz2696@yahoo.com>
2021-10-24 18:25:02 +0800

    Creating dashboard.php, edit css, adding image

---
+++ Project/dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="keywords" content="HTML5, tags"/>
+    <title>Manage module</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css" />
+  </head>
+  
+  <body>
+
+	<div class="container">	
+		<a href="salesmodule.php">
+			<div class="catbox">
+				<img src="image/sales-image.jpg" alt="sales" />
+				<span>Sales Module</span>
+			</div>
+		</a>
+				
+		<a href="inventory.php">
+			<div class="catbox">
+				<img src="image/inventory.jpg" alt="inventory" />
+				<span>Inventory</span>
+			</div>
+		</a>
+				
+		<a href="analysis.php">
+			<div class="catbox">
+				<img src="image/analysis-image.jpg" alt="analysis" />
+				<span>Analysis</span>
+			</div>
+		</a>
+	</div>
+</body>
+</html>
