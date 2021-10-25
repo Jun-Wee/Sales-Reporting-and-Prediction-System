3184ab8ce0b98972579e8357424165a8e29dd3da
Jun-Wee <tjunwee766@gmail.com>
2021-10-25 23:44:56 +0800

    Update usermanual.php

--- Project/usermanual.php
+++ Project/usermanual.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8"/>
-<meta name="keywords" content="HTML5, tags"/>
-<title>User Manual</title>
-<link href="style/style.css" rel="stylesheet" type="text/css" />
-<?php include("include/header.php");?>
+  <title> People Health Pharmacy </title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style/style.css" type="text/css" />
+  <meta name="author" content="" />
 </head>
+<body class = "dashboard">
 
-<h2>How To Use People Health Pharmacy</h2>
 
-
-<body>
+<?php include_once "include/header.php"; ?>
+<h1 class = "para1">
+    User Manual
+</h1>
+<p class = "para2">
+    How to use Sales Module?<br>
+    1.	Sales module is consisted of <strong>Sales Table</strong> and <strong>Add Sales form</strong><br>
+    2.	<strong>Sales Table</strong> will show all the sales record that has been added in PHP sales database.<br>
+    3.	<strong>Add Sales Form</strong> is for employees to input the customer sales data.<br>
+</p>
 <?php include("include/footer.php");?>
 </body>
-</html>
+
+
+
+
