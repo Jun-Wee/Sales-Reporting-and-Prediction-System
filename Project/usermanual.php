8194e652ba2537e4bcdee1d3c6cc5aeeb3d1eb0d
jentz2909 <cjz2696@yahoo.com>
2021-11-07 18:01:36 +0800

    Adding addition details in usermanual

--- Project/usermanual.php
+++ Project/usermanual.php
@@ -18,6 +18,9 @@
     1.	Sales module is consisted of <strong>Sales Table</strong> and <strong>Add Sales form</strong><br>
     2.	<strong>Sales Table</strong> will show all the sales record that has been added in PHP sales database.<br>
     3.	<strong>Add Sales Form</strong> is for employees to input the customer sales data.<br>
+    4.  The <strong> Sales Table </strong> will provide two button for employees to edit or delete the sales record.<br>
+    5.  The <strong>edit button</strong> will allow employee to make any changes on customer sales record.<br>
+    6.  The<strong>delete button</strong> will allow employee to remove record permanently from the database.</br>
 </p>
 <?php include("include/footer.php");?>
 </body>
