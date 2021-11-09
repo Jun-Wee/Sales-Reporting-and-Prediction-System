47949891fe72576f201cc943276f2ce9c9cd400d
jentz2909 <cjz2696@yahoo.com>
2021-11-09 17:29:50 +0800

    modify usermanual and css

--- Project/usermanual.php
+++ Project/usermanual.php
@@ -4,6 +4,7 @@
   <title> People Health Pharmacy </title>
   <meta charset="utf-8" />
   <link rel="stylesheet" href="style/style.css" type="text/css" />
+  
   <meta name="author" content="" />
 </head>
 <body class = "dashboard">
@@ -14,13 +15,33 @@
     User Manual
 </h1>
 <p class = "para2">
-    How to use Sales Module?<br>
-    1.	Sales module is consisted of <strong>Sales Table</strong> and <strong>Add Sales form</strong><br>
-    2.	<strong>Sales Table</strong> will show all the sales record that has been added in PHP sales database.<br>
-    3.	<strong>Add Sales Form</strong> is for employees to input the customer sales data.<br>
-    4.  The <strong> Sales Table </strong> will provide two button for employees to edit or delete the sales record.<br>
-    5.  The <strong>edit button</strong> will allow employee to make any changes on customer sales record.<br>
-    6.  The<strong>delete button</strong> will allow employee to remove record permanently from the database.</br>
+How to use Sales Module?
+<ol class = "usertable">
+  <li>Sales page will display the sales record</li>
+  <li>Add sales table allow to add any missing record</li>
+  <li>Button for edit will allow to make changes if there is an error form the record</li>
+  <li>Beware of the delete button, once it clicked the specify record will permanently remove from the database</li>
+</ol>
+</p>
+<p class = "para2">
+How to use Inventory Module?
+<ol class = "usertable">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ol>
+</p>
+<p class = "para2">
+How to use Analysis Module?
+<ol class = "usertable">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ol>
 </p>
 <?php include("include/footer.php");?>
 </body>
