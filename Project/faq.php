87dc1cd60d0e77aa627bd8e8c726e4f262518b57
Jun-Wee <tjunwee766@gmail.com>
2021-11-11 21:15:59 +0800

    Fix FAQ link and CSS

--- Project/faq.php
+++ Project/faq.php
@@ -12,29 +12,45 @@
 <body class = "dashboard">
 <?php include("include/header.php");?>
 <p class = "question">
-How can view the customer record?
+How can I view the inventory record?
 <ul class = "answer">
 <li>You may click on the inventory module and filter will provided filter to search details</li>
 </ul>
 </p>
+
+<p class = "question">
+How can I view the sales record?
+<ul class = "answer">
+<li>You may navigate to sales module or analysis module for the salas record table </li>
+</ul>
+</p>
+
+
 <p class = "question">
 What if accidentally remove a sales record?
 <ul class = "answer">
 <li>The record will permanantly been deleted, only re-add the sales then it will appear in the table</li>
 </ul>
 </p>
+
+
 <p class = "question">
 What if the pages is not responding?
 <ul class = "answer">
-<li>You may need to check the internet are available or no</li>
+<li>You may need to check with you internet connection.</li>
+<li>It may also due to our routine maintanence.</li>
 </ul>
 </p>
+
+
 <p class = "question">
-What if the record does not been recorded?
+What if the sales record does not be recorded?
 <ul class = "answer">
 <li>You may adding the record manually from the sales module page</li>
 </ul>
 </p>
-<?php include("include/footer.php");?>
+
+
+<?php include("include/footer2.php");?>
 </body>
 </html>
--- Project/include/footer2.php
+++ Project/include/footer2.php
@@ -3,7 +3,7 @@
 <div class="footer-bottom">
     <div class="footer-choose"> 
         <h4><a href="usermanual.php">User Manual</a></h4>
-        <h4>FAQ</h4> 
+        <h4><a href="faq.php">FAQ</a></h4> 
         <h4>Online Shop</f4>
     </div>
     <p> &copy; People Health Pharmacy (PHP) Inc. | Designed by SWE20001 Lab 1 - Group 3</p>
